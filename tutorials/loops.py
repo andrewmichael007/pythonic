05feebb48a54d781b7df1d29d88f33140556ce42
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-24 13:41:22 +0000

    tutorials/loops.py

---
+++ tutorials/loops.py
@@ -0,0 +1,15 @@
+name = "kofi"
+for letter in name:
+    print(letter)
+
+
+names = ["Papa", "kev", "Keisha", "Jake", "Emmett"]
+for name in names:
+    print(name)
+
+tense = 'I love you'
+
+for i in tense:
+    print(i)
+
+

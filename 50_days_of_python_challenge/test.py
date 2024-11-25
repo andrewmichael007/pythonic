2559a8cc24320bf55dd44a8a27a7c307dd1f5e4a
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-25 06:59:51 +0000

    50_days_of_python_challenge/test.py

---
+++ 50_days_of_python_challenge/test.py
@@ -0,0 +1,8 @@
+# A program that takes the name and student's programme
+
+def name_and_programme():
+    name = input("enter name: ")
+    programme = input("enter program: ")
+    return f"Your name is {name} and programme is {programme}"
+print(name_and_programme())
+    

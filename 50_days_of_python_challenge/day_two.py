be3baa27600698a2faf16aa855d1e09f586158dc
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-25 07:26:45 +0000

    50_days_of_python_challenge/day_two.py

---
+++ 50_days_of_python_challenge/day_two.py
@@ -0,0 +1,18 @@
+# strings to integers
+
+
+def convert_add(list1):
+    b = []
+    for i in list1:
+        b.append(int(i))
+    # return b
+    return sum(b)
+
+print(convert_add(['1','3','5']))
+
+
+# name = "kofi"
+# for i in name:
+#     print(i)
+    
+    

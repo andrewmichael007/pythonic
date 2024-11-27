b1613256a0cdc8cac4119a7fa4dea166079cfbe9
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-27 19:20:01 +0000

    updated day_two.py: improved code readability

--- 50_days_of_python_challenge/day_two.py
+++ 50_days_of_python_challenge/day_two.py
@@ -2,11 +2,11 @@
 
 
 def convert_add(list1):
-    b = []
-    for i in list1:
-        b.append(int(i))
+    convert_add_array = []
+    for each in list1:
+        convert_add_array.append(int(each))
     # return b
-    return sum(b)
+    return sum(convert_add_array)
 
 print(convert_add(['1','3','5']))
 
@@ -14,5 +14,8 @@ print(convert_add(['1','3','5']))
 # name = "kofi"
 # for i in name:
 #     print(i)
+
+
+    
     
     

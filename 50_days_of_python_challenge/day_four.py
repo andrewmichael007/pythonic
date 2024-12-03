168a09fb69735a741915a746169c72b416a072ac
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-03 23:31:22 +0000

    improved code readability

--- 50_days_of_python_challenge/day_four.py
+++ 50_days_of_python_challenge/day_four.py
@@ -1,16 +1,20 @@
 # only floats
 
-def only_floats(a, b): #creating a function that takes two arguments.
-    if type(a) == float and type(b) == float: #checking the type of a and b.
+#creating a function that takes two arguments.
+def only_floats(a, b): 
+    #return 2 if first argument is a float and second argument is also a float
+    if type(a) == float and type(b) == float: 
         return 2
+    #return 1 if one of the argument is a float
     elif type(a) == float or type(b) == float:
         return 1
+    #return 0 if none of the argument is  a float
     else:
         return 0
 
-print(only_floats(5, 6))
-print(only_floats(5.0, 6))
-print(only_floats(5.0, 6.0))
+print(only_floats(5, 6))  # 👉 0
+print(only_floats(5.0, 6))  # 👉 1
+print(only_floats(5.0, 6.0)) # 👉 2
 
 
 # def only_floats(a, b):

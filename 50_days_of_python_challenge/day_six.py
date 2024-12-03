dadaf84a0e18a00d6906b04136763290c4327f76
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-03 23:33:20 +0000

    improved code readability

--- 50_days_of_python_challenge/day_six.py
+++ 50_days_of_python_challenge/day_six.py
@@ -1,5 +1,9 @@
+#this tool takes a email address, and displays only the username to the user
+
 def user_name():
+    #prompts user for email
     email = input("enter your email: ")
+    #splits from 0 to the @ symbol
     user_name = email.split('@')[0] 
     return f'Your username is {user_name}'
 

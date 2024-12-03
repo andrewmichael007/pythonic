e15e37e9ea9bbb6ee04ddbe6057cd588dacb93fe
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-03 23:32:30 +0000

    improved code readability

--- 50_days_of_python_challenge/day_five.py
+++ 50_days_of_python_challenge/day_five.py
@@ -1,22 +1,25 @@
-# my discount
+# this program takes a price and dicount and calculates the discounted price.
+# the price and discount takes in numbers only. the program halts if otherwise - the try and catch block takes care of this
+
 
 def my_discount():
     try:
-        # p is the actual price
-        p = int(input("enter price: "))
+        # dynamically takes price from the user
+        price  = int(input("enter price: "))
         print("*************************************\n")
     
-        # d is the discount
-        d = int(input("enter discount:  "))
+        # dynamically takes discount from the user
+        discount = int(input("enter discount:  "))
         print("****************************************\n")
     except ValueError:
+        #displays an error message if anything other than a number was entered
         print("Please enter a digit :)")
         quit()
         
     # price is the discounted price
     # calculating the discounted price
-    price = (p - ((d / 100) * p))
-    return price
+    discounted_price = (price - ((discount / 100) * price))
+    return discounted_price
 
 print(my_discount())
 print("***********************************************")

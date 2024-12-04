0aebea64a815b16fdf17ac00cf669fa5eb1bffcb
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-04 21:35:45 +0000

    improved code readability

--- 50_days_of_python_challenge/day_ten.py
+++ 50_days_of_python_challenge/day_ten.py
@@ -1,8 +1,16 @@
-count = 0
-for numb in [1, 2, 3, 4, 5] :
-    count += numb
-    # print(count)
-print(count)
-    
+# this tool takes a set of numbers and displays the total
+
+#creates a function called sum numbers
+def sum_numbers (numbers):
+    #creats a variable total
+    total = 0
+    #loop through numbers
+    for each in numbers:
+        #calculates total and stores the new value of total still in total
+        total += each 
+    #displays total after each iteration
+    return total
+#displays total after  loop ends
+print(sum_numbers([1,2,3,4,5,6,7,8,9,10]))  # expected output 👉 55
     
     

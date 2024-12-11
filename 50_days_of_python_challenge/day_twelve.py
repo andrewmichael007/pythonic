c526a9064169e055a4b4cb5dc2db7cfd76dfbdc6
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-11 22:22:41 +0000

    added comments

--- 50_days_of_python_challenge/day_twelve.py
+++ 50_days_of_python_challenge/day_twelve.py
@@ -1,14 +1,19 @@
+#import system
 import sys
+#create a function called equal_strings
 def equal_strings():
+    #a try block that catch all valueerrors 
     try:
         string1 = input("enter anything: ")
         print("*******************************************")
         string2 = input("enter something different: ")
         print("*********************************************")   
+    #output a message to tell user to input a string instead.  
     except ValueError: 
         print("no figures next time please.")
+        #exit the program
         sys.exit()
-        
+    #check if length of first string is equal to length of string2
     if len(string1) == len(string2):
         return 'True'
     else:

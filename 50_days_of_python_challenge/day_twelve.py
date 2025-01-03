c3e12cf2f2c020cb95bb2dd94e89a8409a8337dc
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-03 18:03:25 +0000

    a bug to be fixed

--- 50_days_of_python_challenge/day_twelve.py
+++ 50_days_of_python_challenge/day_twelve.py
@@ -1,14 +1,18 @@
+#There's a bug: The program is supposed to display an error message when a user inputs a number
+
 #import system
 import sys
 #create a function called equal_strings
 def equal_strings():
-    #a try block that catch all valueerrors 
+    #a try block that catch all value errors 
     try:
         string1 = input("enter anything: ")
         print("*******************************************")
-        string2 = input("enter something different: ")
+
+        string2 =  input("enter something different: ")
         print("*********************************************")   
     #output a message to tell user to input a string instead.  
+
     except ValueError: 
         print("no figures next time please.")
         #exit the program
@@ -20,4 +24,22 @@ def equal_strings():
         return 'False' 
 
 print(equal_strings())
+
+#Extra challenge
+# This is a program that takes a string 
+# separated by dots as a parameter and counts how many dots are in 
+# the string. For example, "h.e.l.p." should return 4 dots, and 
+# "he.lp." should return 2 dots
+
+#needs to be debugged
+def count_dots(name):
+    count = []
+    for each in name:
+        if each == "e":
+            total = count.append(each)
+    print(count)
+# print(count_dots("michael"))
+
+
+
 

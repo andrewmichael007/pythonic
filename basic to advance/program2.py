3570530a7fe39fa7886703f163e9e6802dc9eba6
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-16 23:49:48 +0000

    code refactoring

--- basic to advance/program2.py
+++ basic to advance/program2.py
@@ -33,4 +33,10 @@ print(result) # program should return 10
 
 
 # TO BE IMPLEMENTED...
+try:
+    first_number  = float(input("enter first number: "))
+    second_number = float(input("enter second number: "))
+except:
+    ValueError
+    print("enter a digit next time.")
 

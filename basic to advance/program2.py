4ec533f86fed962e690053e11c76c28ae11004b3
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-17 11:12:49 +0000

    third implementation of numbers summation

--- basic to advance/program2.py
+++ basic to advance/program2.py
@@ -1,42 +1,46 @@
+import sys
 # # A python program to do arithmetical operations addition and division.
 
 # # addition
 # # first implementation
-first_number = input("enter first number: ") # 5
-second_number = input("enter second number: ") # 5
+# first_number = input("enter first number: ") # 5
+# second_number = input("enter second number: ") # 5
  
-result = first_number + second_number
+# result = first_number + second_number
 # print(result) # program should return 10
 
 # # pitfall : this program outputed 55 instead of 10 because the program just appended the two numbers
 # # suggested solution: convert inputed number to integer
 
 
-# # second implementation
-first_number = int(input("enter first number: ") )# 5
-second_number = int(input("enter second number: ") )# 5
+# # # second implementation
+# first_number = int(input("enter first number: ") )# 5
+# second_number = int(input("enter second number: ") )# 5
  
-result = first_number + second_number
-print(result) # program should return 10
+# result = first_number + second_number
+# print(result) # program should return 10
 
 # pitfall : the code breaks down when a decimal was inputed
 # suggested solution : using float instead of int 
 
-first_number = float(input("enter first number: ") )# 5
-second_number = float(input("enter second number: ") )# 5
+# first_number = float(input("enter first number: ") )# 5
+# second_number = float(input("enter second number: ") )# 5
  
-result = first_number + second_number
-print(result) # program should return 10
+# result = first_number + second_number
+# print(result) # program should return 10
 
 # pitfall : code breaks when any character order than a number is inputed
 # suggested solution : include try and catch block to make sure code still runs if any character other than a number is inputed.
 
 
-# TO BE IMPLEMENTED...
+# Implementation
 try:
     first_number  = float(input("enter first number: "))
     second_number = float(input("enter second number: "))
-except:
-    ValueError
+except ValueError:
     print("enter a digit next time.")
+    sys.exit()
+
+result = first_number + second_number
+print(f"the sum of {first_number} + {second_number}  is  {result}")
 

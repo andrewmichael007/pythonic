9ab725c39a673ec529c4631bc30ca43f9a728e86
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-18 23:57:37 +0000

    started coding area of a triangle program

--- basic to advance/program3.py
+++ basic to advance/program3.py
@@ -1,2 +1,15 @@
 # A program to find the area of a triangle
 
+# input lenght of triangle
+base_of_triangle = float(input("enter the base of the triangle: "))
+
+
+# input height of tirangle
+height_of_triangle = float(input("enter the height of the triangle: "))
+
+
+# formulae for triangle
+# half * base * height
+formulae = 0.5 * base_of_triangle * height_of_triangle
+
+print(f"the area of the triangle is {formulae}")

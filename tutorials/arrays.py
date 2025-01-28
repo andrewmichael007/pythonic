a155d20cfc6529a43f867680b89a14ae3679e84e
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-28 23:56:07 +0000

    added a single line comment

--- tutorials/arrays.py
+++ tutorials/arrays.py
@@ -2,8 +2,11 @@
 chiCast = ["kev", "Papa", "Jemma", "Jake", "Emmmet", "Bakari"]
 print(chiCast[0])
 
-chiCast.append("Keisha")
+chiCast.append("Keisha") #adds keisha to the array.
 # print(chiCast.index("Jake"))
+
+
+#different array methods are goin' to be explored
 
 
 

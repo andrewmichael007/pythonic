44656e34db6b25ea70b19f1bc6545e86b1034ad7
Michael Andrew <adjeimitchel03@gmail.com>
2025-09-24 07:34:01 +0000

    Update second_largest.py

--- dsa_learn/arrays/second_largest.py
+++ dsa_learn/arrays/second_largest.py
@@ -1,1 +1,12 @@
+def second_largest(numbers):
+    temp = numbers[0]
+    for each in numbers:
+        if each > temp:
+            temp = each
+        if each < temp and each > each:
+            temp = each
+        return temp
+# print(second_largest([10, 2, 5, 13, 1])) #👉 10
+print(second_largest([1, 2, 5, 13, 20])) #👉 13
+
 

4fe334a695e75474f7e086b15baea19bb2117194
Michael Andrew <adjeimitchel03@gmail.com>
2025-09-24 07:35:37 +0000

    Update non_zeros.py

--- dsa_learn/arrays/non_zeros.py
+++ dsa_learn/arrays/non_zeros.py
@@ -1,1 +1,10 @@
 
+# numbers = [ 1, 0, 2, 0, 3, 0, 4, 0, 5]
+
+# two pointers, the position and the the item ( each )
+
+# position = 0
+# for each in numbers:
+#     print (each)
+#     if each != 0:
+
